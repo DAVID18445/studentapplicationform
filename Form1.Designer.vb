eab371bbaa75179c4c273c7c277730b91c9728e7
David Witboi <da.ddumba@unik.ac.ug>
2026-09-13 23:21:06 +0300

    Initial commit - Student Application Form

---
+++ Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        DateTimePicker1 = New DateTimePicker()
+        ComboBox1 = New ComboBox()
+        Label8 = New Label()
+        ComboBox2 = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        BackgroundWorker2 = New ComponentModel.BackgroundWorker()
+        GroupBox1 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.MenuHighlight
+        Label1.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(150, 21)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(719, 65)
+        Label1.TabIndex = 0
+        Label1.Text = "STUDENT APPLICATION FORM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(42, 133)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(134, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Student Name"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(524, 133)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(190, 25)
+        Label3.TabIndex = 2
+        Label3.Text = "Registration Number"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(524, 241)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(126, 25)
+        Label4.TabIndex = 3
+        Label4.Text = "Date Of Birth"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(42, 348)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(75, 25)
+        Label5.TabIndex = 4
+        Label5.Text = "Course "
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(524, 348)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(145, 25)
+        Label6.TabIndex = 5
+        Label6.Text = "Phone Number "
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(42, 450)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(131, 25)
+        Label7.TabIndex = 6
+        Label7.Text = "Email Address"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(42, 167)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(407, 59)
+        TextBox1.TabIndex = 7
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(517, 63)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(476, 59)
+        TextBox2.TabIndex = 7
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(512, 391)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(476, 59)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(42, 487)
+        TextBox4.Multiline = True
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(946, 76)
+        TextBox4.TabIndex = 7
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        DateTimePicker1.Location = New Point(512, 288)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(476, 31)
+        DateTimePicker1.TabIndex = 8
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"BSCS", "BBAM", "BSIT"})
+        ComboBox1.Location = New Point(42, 389)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(407, 33)
+        ComboBox1.TabIndex = 9
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(42, 241)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(74, 25)
+        Label8.TabIndex = 2
+        Label8.Text = "Gender"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"MALE", "FEMALE"})
+        ComboBox2.Location = New Point(42, 286)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(407, 33)
+        ComboBox2.TabIndex = 9
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Green
+        Button1.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(42, 589)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(174, 57)
+        Button1.TabIndex = 10
+        Button1.Text = "Apply/Register"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.DodgerBlue
+        Button2.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(391, 589)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(195, 57)
+        Button2.TabIndex = 10
+        Button2.Text = "Clear"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Red
+        Button3.Font = New Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(809, 589)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(179, 57)
+        Button3.TabIndex = 10
+        Button3.Text = "Exit"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.SaddleBrown
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Location = New Point(-5, 104)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(1050, 553)
+        GroupBox1.TabIndex = 11
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.SlateGray
+        ClientSize = New Size(1044, 652)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label8)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents GroupBox1 As GroupBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+        ComboBox2.SelectedIndex = -1
+        DateTimePicker1.Value = DateTime.Now
+        ComboBox1.SelectedIndex = -1
+        TextBox3.Clear()
+        TextBox4.Clear()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MessageBox.Show("Student application submitted successfully!",
+                        "Application Submitted",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Application.Exit()
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+End Class
